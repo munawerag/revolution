ae845e259facc728aa505b00abb0760071881712
atifhussainui <atifhussain@digitalgravity.ae>
2025-01-24 16:20:56 +0500

    product list

--- app/(routes)/project-list/page.tsx
+++ app/(routes)/project-list/page.tsx
@@ -1,5 +1,22 @@
+import SectionHeading from "@/app/components/common/SectionHeading";
+import OurUnique from "@/app/components/ProjectListing/OurUnique";
+import ProjectsList from "@/app/components/ProjectListing/ProjectsList";
+import ProjectVideo from "@/app/components/ProjectListing/ProjectVideo";
+
 const ProjectList = () => {
-  return <></>;
+  return (
+    <>
+      <SectionHeading
+        subTitle="Our Landmarks"
+        title="Completed Projects"
+        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+        isCenter={true}
+      />
+      <ProjectVideo />
+      <ProjectsList />
+      <OurUnique />
+    </>
+  );
 };
 
 export default ProjectList;
---
+++ app/components/common/Footer/FooterLinks/index.tsx
@@ -0,0 +1,54 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+import style from "../FooterLinks/index.module.scss";
+
+type footerLinksProps = {
+  data: any;
+  title: any;
+};
+
+const FooterLinks = ({ data, title }: footerLinksProps) => {
+  const [active, setActive] = useState(false);
+
+  return (
+    <div className={style.footerLinksWrapper}>
+      <div className={style.linksColumn}>
+        <div className={style.linksHead} onClick={() => setActive(!active)}>
+          <span className={style.title}>{title}</span>
+          <div
+            className={`${style.dropdownArrow} ${active ? style.active : ""}`}
+          >
+            <Image
+              src={"/assets/svgs/footer-arrow.svg"}
+              width={9}
+              height={14}
+              alt="arrow"
+            />
+          </div>
+        </div>
+        <div className={`${style.listWrapper} ${active ? style.active : ""}`}>
+          <ul>
+            {data.map((item: any, index: number) => {
+              return (
+                <li key={index}>
+                  <Link
+                    href={item.link || "/"}
+                    target={item?.link?.target || "_self"}
+                    className="w-uline"
+                  >
+                    {item?.title}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FooterLinks;
